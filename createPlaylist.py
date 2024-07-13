69fa22481af3d0c2567f79df8e8963c102406b16
Ryu <130729800+Ryu-Morgan@users.noreply.github.com>
2024-07-13 02:06:29 -0400

    Implemented function that returns artist Id
    Used the Spotify API documentation to implement a function that uses the search endpoint to locate a given artists ID string. This artist ID string is needed to identify the top songs by the specified artist

--- createPlaylist.py
+++ createPlaylist.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
 import os
 import requests
+from sys import argv
 
 
 # Global Variables 
 load_dotenv()
 client_id = os.getenv("CLIENT-ID")
 client_secret = os.getenv("CLIENT-SECRET")
-
 
 
 # Get the token
@@ -40,9 +40,43 @@ def get_token():
     return access_token
 
 
+# Function that returns auth header
+def get_auth_header(token):
+    # According to the Spotify docuemntation any API we make must include the access token
+    return {"Authorization" : "Bearer " + token}
+
+
+# Function that searches for artist and returns the artist id
+def search_spotify_for_artist(token, artist_name):
+    # spotify search endpoint
+    url_search = "https://api.spotify.com/v1/search"
+    # get header 
+    header = get_auth_header(token)
+
+    # identify the most popular artist 
+    query = f"q={artist_name}&type=artist&limit=1"
+
+    # construct the endpoint for search a given artist
+    url_search_for_artist = url_search + "?" + query
+
+    # get json data from endpoint
+    response = requests.get(url=url_search_for_artist,headers=header)
+   
+    result = response.json()
+
+    artist_id = result["artists"]["items"][0]["id"]
+
+    print(artist_id)
+
+
+
 # main
 if __name__ == "__main__":
 
     token = get_token()
 
-    print(token)
+    artist_name = argv[1]
+
+    search_spotify_for_artist(token, artist_name)
+
+    
